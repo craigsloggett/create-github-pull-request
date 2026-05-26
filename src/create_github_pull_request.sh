a5692a4051cd824b5367f022d35bba64e280f41b
Craig Sloggett <craigsloggett@gmail.com>
2026-05-26 15:39:22 -0400

    fix: Use PR title as the required field (#7)
    * fix: Use PR title as the required field
    
    * refactor: Update the action inputs names
    
    * chore: Update input description
    
    * chore: Update summary output
    
    * chore: Update summary output
    
    * chore: Update summary output
    
    * chore: Update summary output

--- src/create_github_pull_request.sh
+++ src/create_github_pull_request.sh
@@ -3,7 +3,7 @@
 set -euf
 
 # Required user inputs.
-: "${COMMIT_MESSAGE:?COMMIT_MESSAGE is required}"
+: "${PULL_REQUEST_TITLE:?PULL_REQUEST_TITLE is required}"
 : "${PULL_REQUEST_HEAD_BRANCH:?PULL_REQUEST_HEAD_BRANCH is required}"
 : "${PULL_REQUEST_BASE_BRANCH:?PULL_REQUEST_BASE_BRANCH is required}"
 : "${GIT_USER_NAME:?GIT_USER_NAME is required}"
@@ -22,16 +22,12 @@ for utility in jq gh; do
 done
 
 # Derived defaults.
-if [ -z "${PULL_REQUEST_TITLE:-}" ]; then
-  PULL_REQUEST_TITLE="$(printf '%s' "${COMMIT_MESSAGE}" | head -n 1)"
-fi
-
-if [ -z "${PULL_REQUEST_BODY:-}" ]; then
-  PULL_REQUEST_BODY="$(
-    printf '%s' "${COMMIT_MESSAGE}" |
-      tail -n +2 |
-      sed '/./,$!d'
-  )"
+if [ -z "${COMMIT_MESSAGE:-}" ]; then
+  if [ -n "${PULL_REQUEST_BODY:-}" ]; then
+    COMMIT_MESSAGE="$(printf '%s\n\n%s\n' "${PULL_REQUEST_TITLE}" "${PULL_REQUEST_BODY}")"
+  else
+    COMMIT_MESSAGE="${PULL_REQUEST_TITLE}"
+  fi
 fi
 
 if [ -z "$(git status --porcelain)" ]; then
@@ -61,5 +57,12 @@ PULL_REQUEST_URL="$(
     --head "${PULL_REQUEST_HEAD_BRANCH}"
 )"
 
-printf 'pull-request-url=%s\n' "${PULL_REQUEST_URL}" >>"${GITHUB_OUTPUT}"
-printf 'pull-request-number=%s\n' "${PULL_REQUEST_URL##*/}" >>"${GITHUB_OUTPUT}"
+{
+  printf 'url=%s\n' "${PULL_REQUEST_URL}"
+  printf 'number=%s\n' "${PULL_REQUEST_URL##*/}"
+} >>"${GITHUB_OUTPUT}"
+
+{
+  printf -- '### Pull Request Created\n\n'
+  printf -- '- URL: %s' "${PULL_REQUEST_URL}"
+} >>"${GITHUB_STEP_SUMMARY}"
